a2a2a8e5d2cef69555fd2105c6bfe9976c524279
Gustavo Chaves <gustavochaves@MacBook-Pro-de-Gustavo.local>
2019-11-12 15:08:04 -0300

    Pacman Animation Setup

---
+++ Assets/Scripts/PacmanController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanController : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 _direction;
+    Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _direction.x = Input.GetAxisRaw("Horizontal");
+        _direction.y = Input.GetAxisRaw("Vertical");
+        _animator.SetFloat("Horizontal", _direction.x);
+        _animator.SetFloat("Vertical", _direction.y);
+
+    }
+}
